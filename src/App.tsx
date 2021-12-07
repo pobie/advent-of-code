d6cefb4ec0cfb0c8d1507443a553b45f750c75eb
Pierre-Olivier Boulet <po.boulet@gmail.com>
2021-12-06 21:02:15 -0500

    Fix basename

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Question from './Views/Question';
 function App() {
   return (
     <Container h="full" w="full" padding="64px" centerContent>
-      <BrowserRouter basename="/advent-of-code-2021">
+      <BrowserRouter basename="/advent-of-code">
         <Routes>
           <Route index element={<Home />} />
           <Route path="question/:id" element={<Question />} />
